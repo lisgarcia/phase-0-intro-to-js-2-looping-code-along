83600407161bf0de2e5672761a3a54094adb36d8
Elisabeth Garcia <lisgarciae@gmail.com>
2023-04-03 17:23:33 -0400

    in progress

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+let names=[`Guadalupe`, `Ollie`, `Aki`]
+
+function writeCards(names) {
+    let cards=[]
+    for (let i = 0; i < names.length; i++) {
+        cards[i]=`Thank you, ${names[i]}, for the wonderful surprise gift!`
+    } return cards
+}
+
